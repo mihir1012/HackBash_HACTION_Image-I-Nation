9305dcca0b22a2dfbe96a1ac67c742e1bd8372f5
smit <developers.comunev@gmail.com>
2021-03-13 21:00:11 +0530

    cors added

--- backend/app.js
+++ backend/app.js
@@ -7,13 +7,13 @@ const bodyparser = require('body-parser');
 const mongoose = require('mongoose')
 var indexRouter = require('./routes/index');
 var usersRouter = require('./routes/users');
-
+var cors = require('cors');
 var app = express();
 
 // view engine setup
 app.set('views', path.join(__dirname, 'views'));
 app.set('view engine', 'jade');
-
+app.use(cors());
 app.use(logger('dev'));
 app.use(express.json());
 app.use(express.urlencoded({extended: false}));
