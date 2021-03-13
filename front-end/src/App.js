82bdd445d4f4e7c58ab718b0452a93cd4447e251
Harsh Maheshwari <mharsh301@gmail.com>
2021-03-13 15:27:14 +0530

    router integrated

--- front-end/src/App.js
+++ front-end/src/App.js
@@ -1,28 +1,16 @@
 import React, { Component } from 'react';
 import LoginScreen from './login/LoginScreen';
-class App extends Component {
-  constructor(props){
-    super(props);
-    this.state={
-      loginPage:[],
-      uploadScreen:[]
-    }
-  }
-  componentWillMount(){
-    var loginPage =[];
-    loginPage.push(<LoginScreen appContext={this} key={"login-screen"}/>);
-    this.setState({
-                  loginPage:loginPage
-                    })
-  }
-  render() {
-    return (
-      <div className="App">
-        {this.state.loginPage}
-        {this.state.uploadScreen}
-      </div>
+import Portal from './portal/portal';
+import {Switch, Route} from 'react-router-dom'
+
+function App() {
+      return (
+        <Switch>
+        <Route exact path='/' component={LoginScreen}/>
+        <Route path='/portal' component={Portal}/>
+      </Switch>
+      
     );
   }
-}
 
 export default App;
